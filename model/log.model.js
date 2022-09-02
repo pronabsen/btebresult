0af7b436d906cc1c306d9eeae1951e1cc0f25fb7
Pronab Sen Gupta <opisengupta@gmail.com>
2022-09-02 15:42:42 +0600

    Update log.model.js

--- model/log.model.js
+++ model/log.model.js
@@ -3,6 +3,10 @@ const mongoose = require("mongoose");
 const logSchema = mongoose.Schema({
   fileName: { type: String, required: true },
   size: { type: Number, required: true },
+  year: { type: Number, required: true },
+  semester: { type: Number, required: true },
+  regulation: { type: Number, required: true },
+  exam_type: { type: String, required: true }
 });
 
 module.exports = Log = mongoose.model("Log", logSchema);
